b83697d8745a5d57801c42c56a7b438a1ad96f4d
Narendra Singh <narendra.sickarwar@gmail.com>
2021-09-17 10:39:42 +0530

    Initial commit

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nazarko/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nazarko/FlutterProjects/CI_CD_Flutter_Demo"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Users/nazarko/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
